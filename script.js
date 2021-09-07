63f7a0f8624bb32796716e5cc5faca69a5bb7871
Zaramant <gtolstov2000@mail.ru>
2021-09-07 16:00:03 +0300

    Нужно переделать таймер

---
+++ script.js
@@ -0,0 +1,86 @@
+"use strict";
+
+const app = () => {
+    const song = document.querySelector('.song'),
+          play = document.querySelector('.play'),
+          outline = document.querySelector('.moving-outline circle'),
+          video = document.querySelector('.vid-container video');
+
+    // Sounds
+    const sounds = document.querySelectorAll('.sound-picker button');
+    // Time Display
+    const timeDisplay = document.querySelector('.time-display');
+    const timeSelect = document.querySelectorAll('.time-select button');
+    // Get the Length of the outline
+    const outlineLength = outline.getTotalLength();
+    console.log(outlineLength);
+    // Duration
+    let fakeDuration = 600;
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset = outlineLength;
+
+    // Pick different sounds
+    sounds.forEach(sound => {
+        sound.addEventListener('click', function() {
+            song.src = this.getAttribute('data-sound');
+            video.src = this.getAttribute('data-video');
+            checkPlaying(song);
+        });
+    });
+
+    // Play sound
+    play.addEventListener('click', () => {
+        checkPlaying(song);
+    });
+
+    // Select time
+    timeSelect.forEach(option => {
+        option.addEventListener('click', function() {
+            fakeDuration = this.getAttribute('data-time');
+            console.log(fakeDuration);
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:
+            ${Math.floor(fakeDuration % 60)}`;
+        });
+    });
+
+    // Create a function specific to stop and play the sound
+    const checkPlaying = (song) => {
+        if (song.paused) {
+            song.play();
+            video.play();
+            play.src = "./svg/pause.svg";
+        } else {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+
+        //We can animated the circle
+        song.ontimeupdate = () => {
+            let currentTime = song.currentTime;
+            let elapsed = fakeDuration - currentTime;
+            let seconds = Math.floor(elapsed % 60);
+            let minutes = Math.floor(elapsed / 60);
+
+            //Animate the circle
+            let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
+            outline.style.strokeDashoffset = progress;
+
+            // Animate the text
+            timeDisplay.textContent = `${minutes}:${seconds}`;
+
+            if (currentTime >= fakeDuration) {
+                song.pause();
+                song.currentTime = 0;
+                play.src = "./svg/play.svg";
+                video.pause();
+            }
+        };
+    };
+
+};
+
+
+
+app();
